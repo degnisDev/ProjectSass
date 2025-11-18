c9ff23db73012aae437cd0ed3325225823a6366f
degnisDev <degnisdev@gmail.com>
2025-11-18 08:52:45 -0500

    Agregamos y migramos los estilos

--- proyecto-pelis-angular/src/app/app.routes.ts
+++ proyecto-pelis-angular/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Inicio } from './pages/inicio/inicio';
+import { Contactos } from './pages/contactos/contactos';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: '', component: Inicio },
+	{ path: 'contactos', component: Contactos },
+	{ path: '**', redirectTo: '' }
+];
